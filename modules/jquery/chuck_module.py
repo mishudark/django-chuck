a1bf3326522f36c2c1e8dc707e2812d8ddf8229a
lukasbuenger <lukasbuenger@gmail.com>
2012-06-06 22:09:18 +0200

    jquery module fixed. simple curl from code.jquery.com. closes #4

--- modules/jquery/chuck_module.py
+++ modules/jquery/chuck_module.py
@@ -2,23 +2,28 @@ import subprocess
 import os
 
 description = """
-Installs the jQuery javascript library into the static folders.
-
-Please note, that this module requires Git in order to work properly.
+Installs the latest jQuery version into the static folders at:
+<STATIC_URL>/scripts/libs/jquery/jquery.js
+<STATIC_URL>/scripts/libs/jquery/jquery.min.js
 
 For more information, visit:
 http://jquery.org/
 """
 
 def post_build():
+
     jquery_dir = os.path.join(project_dir, 'static/scripts/libs/jquery')
     if not os.path.exists(jquery_dir):
-            os.makedirs(jquery_dir)
-    subprocess.call(
-        'cd '+site_dir+'; mkdir .src; cd .src; \
-        git clone git://github.com/jquery/jquery.git; \
-        cd jquery; \
-        make; \
-        mv -v dist/* '+jquery_dir+'; \
-        cd '+site_dir+'; rm -rf .src;',
-        shell=True)
+        os.makedirs(jquery_dir)
+    commands = [
+        'cd '+jquery_dir,
+        'touch jquery.js',
+        'touch jquery.min.js',
+        'curl http://code.jquery.com/jquery-latest.js > jquery.js',
+        'curl http://code.jquery.com/jquery-latest.min.js > jquery.min.js',
+        ]
+    kwargs = dict(
+        shell=True
+    )
+    process = subprocess.Popen('; '.join(commands), **kwargs)
+    stdout, stderr = process.communicate()
