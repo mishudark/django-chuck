160f41356826910f2955408d5597f4167bb6c567
Bastian Ballmann <bastian.ballmann@notch-interactive.com>
2012-06-13 16:27:01 +0200

    test script

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# run unit tests
+cd django_chuck
+python test.py
+
+cd django_chuck/template/notch_interactive
+python test.py
+
+
+# test commands
+chuck list_modules
+chuck search_module cms
+chuck show_info fabric
+chuck create_project test test
