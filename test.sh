2d179063e33ba04dd0973725c600b4cb089b13cf
Bastian Ballmann <bastian.ballmann@notch-interactive.com>
2012-06-14 14:03:11 +0200

    more tests

--- test.sh
+++ test.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 # run unit tests
-python django_chuck/test.py
-python django_chuck/commands/test.py
-python django_chuck/template/notch_interactive/test.py
+python django_chuck/test.py || exit 1
+python django_chuck/commands/test.py  || exit 1
+python django_chuck/template/notch_interactive/test.py || exit 1
 
 
 # test commands
-chuck list_modules
-chuck search_module cms
-chuck show_info fabric
-chuck create_project test test
+chuck list_modules || exit 1
+chuck search_module cms || exit 1
+chuck show_info fabric || exit 1
+chuck create_project test test || exit 1
+chuck create_project test test django-cms south uwsgi nginx || exit 1
