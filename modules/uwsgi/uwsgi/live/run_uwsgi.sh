0b0ea49a88815dd36918c248e54d41f87ea4b2f5
mishudark <mishudark@moon>
2012-11-06 23:45:27 +0000

    delete sock before run

--- modules/uwsgi/uwsgi/live/run_uwsgi.sh
+++ modules/uwsgi/uwsgi/live/run_uwsgi.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -f /var/run/uwsgi/$SITE_NAME.sock
 uwsgi -p 1 -C -A 4 -m -s /var/run/uwsgi/$SITE_NAME.sock \
  --wsgi-file $PROJECT_BASEDIR/$SITE_NAME/django.wsgi \
  --pythonpath $PROJECT_BASEDIR/$SITE_NAME/$PROJECT_NAME \
